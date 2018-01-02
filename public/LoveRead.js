63f5e029b8005e79f31818e6ad3b8cfb0d4b3ba4
hsuannn <didi.ya35@gmail.com>
2018-01-02 16:05:11 +0800

    s2

--- public/LoveRead.js
+++ public/LoveRead.js
@@ -2,7 +2,7 @@ $(document).ready(()=>{ // jQuery main
 
     const stage = new createjs.Stage(canvas);
     const repo = new createjs.LoadQueue();
-    let level = 0;
+    let level = 2;
     let scene = 0;
 
     function setup() {
@@ -32,11 +32,33 @@ $(document).ready(()=>{ // jQuery main
             {id:'stage1Text',src:'Stage1/images/stage1_text_.png'},
 
             {id:'heartText',src:'Stage1/images/heart_text.png'},
-            {id:'heart',src:'Stage1/images/heart1.png'},
+            //{id:'heart',src:'Stage1/images/heart1.png'},
 
             {id:'background',src:"Stage1/sound/background_music.mp3"},
             {id:'heartbeat',src:"Stage1/sound/heartbeats.mp3"},
+
             // Stage 2
+            {id:'s2', src:'Stage2/images/stage2.png'},
+            //{id:'s2_text', src:'Stage2/images/stage2_text.png'},
+            {id:'heart_text', src:'Stage2/images/heart_text.png'},
+            {id:'life', src:'Stage2/images/heart.png'},
+            {id:'burp', src:'Stage2/images/burp.PNG'},
+            {id:'rude', src:'Stage2/images/rude.PNG'},
+            {id:'girle1', src:'Stage2/images/gfEating1.PNG'},
+            {id:'girle2', src:'Stage2/images/gfEating2.PNG'},
+            {id:'girlm1', src:'Stage2/images/gfMad1.PNG'},
+            {id:'girlm2', src:'Stage2/images/gfMad2.PNG'},
+            {id:'boye1', src:'Stage2/images/bfBurp1.PNG'},
+            {id:'boye2', src:'Stage2/images/bfBurp3.PNG'},
+            {id:'boye3', src:'Stage2/images/bfBurp5.PNG'},
+            {id:'boye4', src:'Stage2/images/bfBurp7.PNG'},
+            {id:'boye5', src:'Stage2/images/bfBurp9.PNG'},
+            {id:'boyb1', src:'Stage2/images/bfBurp2.PNG'},
+            {id:'boyb2', src:'Stage2/images/bfBurp4.PNG'},
+            {id:'boyb3', src:'Stage2/images/bfBurp6.PNG'},
+            {id:'boyb4', src:'Stage2/images/bfBurp8.PNG'},
+            //{id:'CHfont', src:'Stage2/images/CSong3HK.otf'},
+            {id:'burp_sound', src:'Stage2/sound/burp1.mp3'},
 
             // Stage 3
             {id:'blood',src:"Stage3/images/blood.png"},
@@ -72,6 +94,16 @@ $(document).ready(()=>{ // jQuery main
 
         // var playerName;
         // var otherName;
+        if (window.otherName == null) window.otherName = 'G';
+
+        let life = [];
+        for (let i = 0; i < 6; i++) {
+            life[i] = new createjs.Bitmap(repo.getResult('life'));
+            life[i].set({x: canvas.width - life[i].image.width * 1.1 * (6 - i), y: 5});
+        }
+
+        let score = 2;
+        let scoreChange = false;
 
         if(level === 0) {
             if (scene === 0) {
@@ -1115,7 +1147,155 @@ $(document).ready(()=>{ // jQuery main
                 });
             }
         }else if(level === 2) {
-            win();
+            let isWin = true;
+            //createjs.FontLoader(repo.getResult('CHfont'));
+            let s2_text = new createjs.Text(
+                "你 約  "+otherName+"  去 吃 浪 漫 晚 餐\n" +
+                "吃 飽 後 你 很 想 打 個 嗝\n" +
+                "請 選 擇 在 正 確 的 時 機 點 解 放", "40px CSong3HK", "#000000");
+            s2_text.set({textAlign:'center', lineHeight:70, scaleX: 0.8});
+            s2_text.set({x: 360, y: 250});
+
+            createjs.Tween.get(s2_text).wait(5000).call( function(){
+                let time_loop;
+                stage.removeChild(s2_text);
+
+                let s2 = new createjs.Bitmap(repo.getResult('s2'));
+                s2.set({y: 3, scaleX: 1.44, scaleY: 1.44});
+                stage.addChild(s2); // ＳＴＡＧＥ ２
+
+                for (let i = 0; i < score; i++) {
+                    stage.addChild(life[i]); // hearts
+                }
+
+                let heart_text = new createjs.Bitmap(repo.getResult('heart_text'));
+                heart_text.set({
+                    x: canvas.width - heart_text.image.width * 1.35 - 55 * 6, y: -13,
+                    scaleX: 1.35, scaleY: 1.35
+                });
+                stage.addChild(heart_text); // 好感度
+
+                let girle = [new createjs.Bitmap(repo.getResult('girle1')),
+                    new createjs.Bitmap(repo.getResult('girle2'))];
+                for (let girl of girle) {
+                    girl.set({x: 500, y: 250, scaleX: 1.5, scaleY: 1.5});
+                }
+
+                let girl_count = 0;
+                stage.addChild(girle[0]);
+                let girl_loop = createjs.Tween.get(girle, {loop: true}).wait(1000).call(function () {
+                    stage.removeChild(girle[girl_count]);
+                    girl_count = (girl_count + 1) % 2;
+                    stage.addChild(girle[girl_count]);
+                });
+
+                let girlm = [new createjs.Bitmap(repo.getResult('girlm1')),
+                    new createjs.Bitmap(repo.getResult('girlm2'))];
+                for (let girl of girlm) {
+                    girl.set({x: 500, y: 250, scaleX: 1.5, scaleY: 1.5});
+                }
+
+                let rude = new createjs.Bitmap(repo.getResult('rude'));
+                rude.set({x: 510, y: 120, scaleX: 2, scaleY: 2});
+
+                let burp = new createjs.Bitmap(repo.getResult('burp'));
+                burp.set({x: 240, y: 280, scaleX: 1.5, scaleY: 1.5});
+
+                let boye = [new createjs.Bitmap(repo.getResult('boye1')),
+                    new createjs.Bitmap(repo.getResult('boye2')),
+                    new createjs.Bitmap(repo.getResult('boye3')),
+                    new createjs.Bitmap(repo.getResult('boye4')),
+                    new createjs.Bitmap(repo.getResult('boye5'))];
+                for (let boy of boye) {
+                    boy.set({scaleX: 1.5, scaleY: 1.5, regY: boy.image.height, x: 100, y: 460});
+                }
+
+                let boyb = [new createjs.Bitmap(repo.getResult('boyb1')),
+                    new createjs.Bitmap(repo.getResult('boyb2')),
+                    new createjs.Bitmap(repo.getResult('boyb3')),
+                    new createjs.Bitmap(repo.getResult('boyb4')),
+                    new createjs.Bitmap(repo.getResult('boye5'))];
+                for (let boy of boyb) {
+                    boy.set({scaleX: 1.5, scaleY: 1.5, regY: boy.image.height, x: 100, y: 460});
+                }
+
+                let boy_count = 0;
+                let burp_count = 0;
+                stage.addChild(boye[0]);
+                let boy_loop = createjs.Tween.get(boye, {loop: true}).wait(700).call(function () {
+                    stage.removeChild(boye[boy_count]);
+
+                    window.addEventListener('keydown', function (e) {
+                        switch (e.keyCode) {
+                            case 66: //space
+                                stage.removeChild(boye[boy_count]);
+                                stage.addChild(boyb[boy_count]);
+                                stage.addChild(burp);
+                                repo.getResult('burp_sound').play();
+                                burp_count += 1;
+                                if (boy_count == 0 || boy_count == 1 || burp_count >= 10) {
+                                    isWin = false;
+                                    stage.removeChild(girle[girl_count]);
+                                    girl_loop.setPaused(true);
+                                    stage.removeChild(girlm[1]);
+                                    stage.addChild(girlm[0]);
+                                    createjs.Tween.get(girlm).wait(800).call(function () {
+                                        stage.removeChild(girlm[0]);
+                                        stage.addChild(girlm[1]);
+                                    });
+                                    stage.addChild(rude);
+                                }
+                                stage.removeChild(illu_text);
+                                time_loop.setPaused(true);
+                                boy_loop.setPaused(true);
+                                pressToNext(score,isWin,scoreChange);
+                        }
+                    });
+
+                    boy_count = (boy_count + 1) % 5;
+                    stage.addChild(boye[boy_count]);
+                });
+
+                let s2_time = 30;
+                let illu_text = new createjs.Text("按下‘b’鍵打嗝\n" +
+                    "還剩 " + s2_time + " 秒", "16px Arial", "#c4322e");
+                illu_text.set({textAlign: 'center', lineHeight:24, x: 160, y: 150});
+                stage.addChild(illu_text);
+
+                time_loop = createjs.Tween.get(illu_text, {loop: true}).wait(1000).call(function () {
+                    stage.removeChild(illu_text);
+                    s2_time -= 1;
+                    illu_text.text = "按下‘b’鍵打嗝\n" + "還剩 " + s2_time + " 秒";
+                    if (s2_time > 0) {
+                        stage.addChild(illu_text);
+                    } else {
+                        repo.getResult('burp_sound').play();
+                        stage.removeChild(boye[boy_count]);
+                        boy_loop.setPaused(true);
+                        stage.addChild(boyb[boy_count]);
+                        stage.addChild(burp);
+                        if (boy_count == 1 || boy_count == 0) {
+                            isWin = false;
+                            stage.removeChild(girle[girl_count]);
+                            girl_loop.setPaused(true);
+                            stage.removeChild(girlm[1]);
+                            stage.addChild(girlm[0]);
+                            createjs.Tween.get(girlm).wait(800).call(function () {
+                                stage.removeChild(girlm[0]);
+                                stage.addChild(girlm[1]);
+                            });
+                            stage.addChild(rude);
+                        }
+                        stage.removeChild(illu_text);
+                        time_loop.setPaused(true);
+                        time_loop.setPaused(true);
+                        pressToNext(score,isWin,scoreChange);
+                    }
+                });
+
+            } );
+            stage.addChild(s2_text);
+
         }else if(level === 3) {
             let carRoad = repo.getResult('carRoad');
             let clapping = repo.getResult('clapping');
@@ -1391,6 +1571,33 @@ $(document).ready(()=>{ // jQuery main
             //end
 
         }
+
+        function pressToNext(score,isWin,scoreChanged) {
+            let bg = new createjs.Shape();
+            bg.graphics.beginFill('#ffffff').drawRect(0,490,720,100);
+            stage.addChild(bg);
+            let text = new createjs.Text("按下‘enter’鍵前往下一關", "16px Arial", "#c4322e");
+            text.set({textAlign: 'center', x: 360, y: 500});
+            stage.addChild(text);
+            if (!scoreChanged) {
+                if (!isWin) score--;
+                else score++;
+                scoreChanged = true;
+            }
+            console.log(score);
+            for (let i = 0; i < 6; i++) {
+                if (i < score) stage.addChild(life[i]); // hearts
+                else stage.removeChild(life[i])
+            }
+            window.addEventListener('keydown', function (e) {
+                switch (e.keyCode) {
+                    case 13: //enter
+                        win();
+                        scoreChanged = false;
+                }
+            });
+        }
+
     }
 
     function isHit(ax,ay, aw,ah, bx,by, bw,bh) {
--- public/Stage2/Stage2.js
+++ public/Stage2/Stage2.js
@@ -27,57 +27,60 @@ $(document).ready(()=>{ // jQuery main
             {id:'boyb2', src:'images/bfBurp4.PNG'},
             {id:'boyb3', src:'images/bfBurp6.PNG'},
             {id:'boyb4', src:'images/bfBurp8.PNG'},
-            {id:'CHfont', src:'images/CSong3HK.otf'},
+            //{id:'CHfont', src:'images/CSong3HK.otf'},
             {id:'burp_sound', src:'sound/burp1.mp3'} ]);
         repo.on('complete', draw);
     }
 	
 	function draw() {
-        let CHfont = new createjs.FontLoader(repo.getResult('CHfont'));
+        //createjs.FontLoader(repo.getResult('CHfont'));
         let s2_text = new createjs.Text(
-            "你 約 "+otherName+" 去 吃 浪 漫 晚 餐\n" +
+            "你 約 "+'otherName'+" 去 吃 浪 漫 晚 餐\n" +
             "吃 飽 後 你 很 想 打 個 嗝\n" +
-            "請 選 擇 在 正 確 的 時 機 點 解 放", "40px CHfont", "#000000");
+            "請 選 擇 在 正 確 的 時 機 點 解 放", "40px CSong3HK", "#000000");
         s2_text.set({textAlign:'center', lineHeight:70, scaleX: 0.8});
         s2_text.set({x: 360, y: 250});
 
         createjs.Tween.get(s2_text).wait(500).call( function(){
+            let time_loop;
             stage.removeChild(s2_text);
 
             let s2 = new createjs.Bitmap(repo.getResult('s2'));
-            s2.set({y:3, scaleX:1.44, scaleY:1.44});
+            s2.set({y: 3, scaleX: 1.44, scaleY: 1.44});
             stage.addChild(s2); // ＳＴＡＧＥ ２
 
-            for (let i=0; i<score; i++) {
+            for (let i = 0; i < score; i++) {
                 let heart = new createjs.Bitmap(repo.getResult('heart'));
-                heart.set({ x: canvas.width - heart.image.width * 1.1 * (6-i), y:5 });
+                heart.set({x: canvas.width - heart.image.width * 1.1 * (6 - i), y: 5});
                 stage.addChild(heart); // hearts
             }
 
             let heart_text = new createjs.Bitmap(repo.getResult('heart_text'));
-            heart_text.set({x:canvas.width - heart_text.image.width * 1.35 - 55 * 6, y:-13,
-                scaleX:1.35, scaleY:1.35});
+            heart_text.set({
+                x: canvas.width - heart_text.image.width * 1.35 - 55 * 6, y: -13,
+                scaleX: 1.35, scaleY: 1.35
+            });
             stage.addChild(heart_text); // 好感度
 
-            let illu_text = new createjs.Text("按下‘b’鍵打嗝", "20px Arial", "#c4322e");
-            illu_text.set({textAlign:'center', x:360, y:150});
-            stage.addChild(illu_text);
-
-            let girle = [ new createjs.Bitmap(repo.getResult('girle1')),
-                new createjs.Bitmap(repo.getResult('girle2')) ];
-            for (let girl of girle) { girl.set({x: 500, y: 250, scaleX: 1.5, scaleY: 1.5}); }
+            let girle = [new createjs.Bitmap(repo.getResult('girle1')),
+                new createjs.Bitmap(repo.getResult('girle2'))];
+            for (let girl of girle) {
+                girl.set({x: 500, y: 250, scaleX: 1.5, scaleY: 1.5});
+            }
 
             let girl_count = 0;
             stage.addChild(girle[0]);
-            girl_loop = createjs.Tween.get(girle, {loop:true}).wait(1000).call( function() {
+            let girl_loop = createjs.Tween.get(girle, {loop: true}).wait(1000).call(function () {
                 stage.removeChild(girle[girl_count]);
-                girl_count = (girl_count+1) % 2;
+                girl_count = (girl_count + 1) % 2;
                 stage.addChild(girle[girl_count]);
             });
 
-            let girlm = [ new createjs.Bitmap(repo.getResult('girlm1')),
-                new createjs.Bitmap(repo.getResult('girlm2')) ];
-            for (let girl of girlm) { girl.set({x: 500, y: 250, scaleX: 1.5, scaleY: 1.5}); }
+            let girlm = [new createjs.Bitmap(repo.getResult('girlm1')),
+                new createjs.Bitmap(repo.getResult('girlm2'))];
+            for (let girl of girlm) {
+                girl.set({x: 500, y: 250, scaleX: 1.5, scaleY: 1.5});
+            }
 
             let rude = new createjs.Bitmap(repo.getResult('rude'));
             rude.set({x: 510, y: 120, scaleX: 2, scaleY: 2});
@@ -85,24 +88,28 @@ $(document).ready(()=>{ // jQuery main
             let burp = new createjs.Bitmap(repo.getResult('burp'));
             burp.set({x: 240, y: 280, scaleX: 1.5, scaleY: 1.5});
 
-            let boye = [ new createjs.Bitmap(repo.getResult('boye1')),
+            let boye = [new createjs.Bitmap(repo.getResult('boye1')),
                 new createjs.Bitmap(repo.getResult('boye2')),
                 new createjs.Bitmap(repo.getResult('boye3')),
                 new createjs.Bitmap(repo.getResult('boye4')),
-                new createjs.Bitmap(repo.getResult('boye5')) ];
-            for (let boy of boye) { boy.set({scaleX: 1.5, scaleY: 1.5, regY: boy.image.height, x: 100, y: 460}); }
+                new createjs.Bitmap(repo.getResult('boye5'))];
+            for (let boy of boye) {
+                boy.set({scaleX: 1.5, scaleY: 1.5, regY: boy.image.height, x: 100, y: 460});
+            }
 
-            let boyb = [ new createjs.Bitmap(repo.getResult('boyb1')),
+            let boyb = [new createjs.Bitmap(repo.getResult('boyb1')),
                 new createjs.Bitmap(repo.getResult('boyb2')),
                 new createjs.Bitmap(repo.getResult('boyb3')),
                 new createjs.Bitmap(repo.getResult('boyb4')),
-                new createjs.Bitmap(repo.getResult('boye5')) ];
-            for (let boy of boyb) { boy.set({scaleX: 1.5, scaleY: 1.5, regY: boy.image.height, x: 100, y: 460}); }
+                new createjs.Bitmap(repo.getResult('boye5'))];
+            for (let boy of boyb) {
+                boy.set({scaleX: 1.5, scaleY: 1.5, regY: boy.image.height, x: 100, y: 460});
+            }
 
             let boy_count = 0;
             let burp_count = 0;
             stage.addChild(boye[0]);
-            boy_loop = createjs.Tween.get(boye, {loop:true}).wait(1000).call( function() {
+            let boy_loop = createjs.Tween.get(boye, {loop: true}).wait(500).call(function () {
                 stage.removeChild(boye[boy_count]);
 
                 window.addEventListener('keydown', function (e) {
@@ -112,48 +119,68 @@ $(document).ready(()=>{ // jQuery main
                             stage.addChild(boyb[boy_count]);
                             stage.addChild(burp);
                             repo.getResult('burp_sound').play();
-                            console.log(burp_count);
-                            if ( boy_count == 0 || burp_count >= 5 ) {
+                            burp_count += 1;
+                            if (boy_count == 0 || boy_count == 1 || burp_count >= 10) {
+                                stage.removeChild(girle[girl_count]);
+                                girl_loop.setPaused(true);
+                                stage.removeChild(girlm[1]);
+                                stage.addChild(girlm[0]);
+                                createjs.Tween.get(girlm).wait(800).call(function () {
+                                    stage.removeChild(girlm[0]);
+                                    stage.addChild(girlm[1]);
+                                });
                                 stage.addChild(rude);
                             }
+                            stage.removeChild(illu_text);
+                            time_loop.setPaused(true);
                             boy_loop.setPaused(true);
                     }
                 });
-                window.addEventListener('keyup', function (e) {
-                    switch (e.keyCode) {
-                        case 66: //space
-                            burp_count+=1;
-                    }
-                });
 
-                boy_count = (boy_count+1) % 5;
+                boy_count = (boy_count + 1) % 5;
                 stage.addChild(boye[boy_count]);
+            });
+
+            let s2_time = 30;
+            let illu_text = new createjs.Text("按下‘b’鍵打嗝\n" +
+                "還剩 " + s2_time + " 秒", "16px CHfont", "#c4322e");
+            illu_text.set({textAlign: 'center', lineHeight:24, x: 160, y: 150});
+            stage.addChild(illu_text);
+
+            time_loop = createjs.Tween.get(illu_text, {loop: true}).wait(1000).call(function () {
+                stage.removeChild(illu_text);
+                s2_time -= 1;
+                illu_text.text = "按下‘b’鍵打嗝\n" + "還剩 " + s2_time + " 秒";
+                if (s2_time > 0) {
+                    stage.addChild(illu_text);
+                } else {
+                    repo.getResult('burp_sound').play();
+                    stage.removeChild(boye[boy_count]);
+                    boy_loop.setPaused(true);
+                    stage.addChild(boyb[boy_count]);
+                    stage.addChild(burp);
+                    if (boy_count == 1 || boy_count == 0) {
+                        stage.removeChild(girle[girl_count]);
+                        girl_loop.setPaused(true);
+                        stage.removeChild(girlm[1]);
+                        stage.addChild(girlm[0]);
+                        createjs.Tween.get(girlm).wait(800).call(function () {
+                            stage.removeChild(girlm[0]);
+                            stage.addChild(girlm[1]);
+                        });
+                        stage.addChild(rude);
+                    }
+                    stage.removeChild(illu_text);
+                    time_loop.setPaused(true);
+                    time_loop.setPaused(true);
+                }
             });
 
         } );
         stage.addChild(s2_text);
 
-
-        tank1.on('click', function(e) {
-        	let dot = new createjs.Shape();
-        	dot.graphics.beginFill('red').drawCircle(tank1.x+22, tank1.y-9,5);
-        	createjs.Tween.get(dot).to({x:300},1500).call(function () {
-				stage.removeChild(dot);//子彈不見
-        	});
-        	stage.addChild(dot);
-        });
-
-        tank1.on('mousedown', e =>{
-        	stage.on('stagemousemove', e =>{
-        		tank1.x= stage.mouseX;
-        		tank1.y= stage.mouseY;
-        	});
-        	stage.on('stagemouseup', e =>{
-        		e.target.removeAllEventListeners();
-        	});
-        });
     }
-	
+
 	setup();
 	
 });
